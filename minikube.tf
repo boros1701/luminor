fd83445f9a31143d3b19ed4e908d7fb5eec02623
boros1701 <97278953+boros1701@users.noreply.github.com>
2023-11-18 00:16:49 +0200

    Update minikube.tf

--- minikube.tf
+++ minikube.tf
@@ -1,14 +1,67 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.11.0"
-    }
-  }
+provider "minikube" {
+  kubernetes_version = "v1.26.3"
+}
+
+resource "minikube_cluster" "docker" {
+  driver       = "docker"
+  cluster_name = "terraform-provider-minikube-acc-docker"
+  addons = [
+    "default-storageclass",
+    "storage-provisioner"
+  ]
 }
 
 provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = "minikube"
+  host = minikube_cluster.docker.host
+
+  client_certificate     = minikube_cluster.docker.client_certificate
+  client_key             = minikube_cluster.docker.client_key
+  cluster_ca_certificate = minikube_cluster.docker.cluster_ca_certificate
 }
 
+
+resource "kubernetes_deployment" "deployment" {
+  metadata {
+    name = "nginx-example"
+    labels = {
+      App = "NginxExample"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "NginxExample"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "NginxExample"
+        }
+      }
+      spec {
+        container {
+          image = "nginx:1.7.8"
+          name  = "example"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
