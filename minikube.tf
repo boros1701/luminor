6fc1758ad2e9556ad70d82fde45b3bf09db05384
boros1701 <97278953+boros1701@users.noreply.github.com>
2023-11-17 23:47:18 +0200

    Update minikube.tf

--- minikube.tf
+++ minikube.tf
@@ -2,16 +2,11 @@ provider "minikube" {
   kubernetes_version = "v1.26.3"
 }
 
-resource "minikube_cluster" "hyperkit" {
-  vm           = true
-  driver       = "hyperkit"
-  cluster_name = "terraform-provider-minikube-acc-hyperkit"
-  nodes        = 3
-  cni          = "bridge" # Allows pods to communicate with each other via DNS
+resource "minikube_cluster" "docker" {
+  driver       = "docker"
+  cluster_name = "terraform-provider-minikube-acc-docker"
   addons = [
-    "dashboard",
     "default-storageclass",
-    "ingress",
     "storage-provisioner"
   ]
 }
